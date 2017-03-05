d002f9f12e9787bb6ec6e87fbb39d9251c73c85e
uzair shahid <uzairshahid7111@gmail.com>
2017-03-05 23:21:11 +0500

    hello world

---
+++ myfirstprogram/app.js
@@ -0,0 +1,10 @@
+
+
+var a = "786";
+var input  ="hello";
+alert(a);
+alert(input)
+alert(a + "           "+ input)
+alert("there is 1 alert for you  :)")
+alert("there is another alert for you ")
+alert("ok bye.....!   :)")
